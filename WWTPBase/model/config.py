cd51bdf7c9085fbc5c64ff9dac5ec41f65d6557f
xdrl1 <xuanshu.luo@live.com>
2023-11-08 13:29:15 +0100

    Wubbalubbadubdub!!

--- WWTPBase/model/config.py
+++ WWTPBase/model/config.py
@@ -7,8 +7,6 @@ from detectron2.data.datasets import register_coco_instances
 from . import PATH_MODEL_OUTPUT
 from .. import PATH_TR_IMG_DIR, PATH_VA_IMG_DIR, PATH_TE_IMG_DIR, \
                PATH_TR_LABEL,   PATH_VA_LABEL,   PATH_TE_LABEL
-# from ..utils.File import LoadPKL
-
 
 register_coco_instances("WWTP_tr", {}, PATH_TR_LABEL, PATH_TR_IMG_DIR)
 register_coco_instances("WWTP_va", {}, PATH_VA_LABEL, PATH_VA_IMG_DIR)
@@ -16,7 +14,7 @@ register_coco_instances("WWTP_te", {}, PATH_TE_LABEL, PATH_TE_IMG_DIR)
 
 
 def GetBaseConfig(_loadTrainedModel=False, _checkPointModel="model_final.pth"):
-    pretrained_cfg = "COCO-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_3x.yaml"  # from model zoo
+    pretrained_cfg = "COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml"  # from model zoo
     cfg = get_cfg()
     cfg.OUTPUT_DIR = PATH_MODEL_OUTPUT
     cfg.INPUT.FORMAT = "RGB"  # for statistics
@@ -25,16 +23,16 @@ def GetBaseConfig(_loadTrainedModel=False, _checkPointModel="model_final.pth"):
     cfg.MODEL.PIXEL_MEAN = [90.39885420874151, 99.5118328394066, 88.92560153182265]
     cfg.MODEL.PIXEL_STD = [49.071554532775444, 41.866186751015704, 38.826466744058635]
 
-    cfg.DATASETS.TRAIN = ("icelake_tr",)
+    cfg.DATASETS.TRAIN = ("WWTP_tr",)
     cfg.DATASETS.TEST = ()
     cfg.DATALOADER.NUM_WORKERS = 1
     cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(pretrained_cfg)
     if _loadTrainedModel:
         cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, _checkPointModel)
     cfg.SOLVER.IMS_PER_BATCH = 32  # real batch size
-    cfg.SOLVER.BASE_LR = 0.00008  # LR
-    cfg.SOLVER.MAX_ITER = 10000    # epoch
-    cfg.MODEL.RPN.BATCH_SIZE_PER_IMAGE = 3584
-    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 3584   # The "RoIHead batch size". 128 is faster, and good enough for this toy dataset (default: 512)
+    cfg.SOLVER.BASE_LR = 0.008  # LR
+    cfg.SOLVER.MAX_ITER = 1000    # epoch
+    cfg.MODEL.RPN.BATCH_SIZE_PER_IMAGE = 1024
+    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 1024   # The "RoIHead batch size". 128 is faster, and good enough for this toy dataset (default: 512)
     cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (icelake)
     return cfg
